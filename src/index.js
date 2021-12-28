3bdd286a1d5c180d34687cf26f767c2c4f99326c
João Amadeu <jmamadeu2000@gmail.com>
2021-12-28 17:30:22 +0100

    chore: make tests passing

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(u => u.username === username)
+
+  if(!user) {
+    return response.status(404).json({error: true})
+  }
+  
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if((!user.pro && user.todos.length === 10)) {
+    return response.status(403).json({error: true})
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  if(!validate(id)) {
+    return response.status(400).json({error: true})
+  }
+
+  const user = users.find(u => u.username === username)
+
+  const userTodo = user?.todos.find(td => td.id === id)
+
+  if(!userTodo) {
+    return response.status(404).json({error: true})
+  }
+
+  request.user = user
+  request.todo = userTodo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(u => u.id === id)
+
+  if(!user) {
+    return response.status(404).json({error: true})
+  }
+  
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
